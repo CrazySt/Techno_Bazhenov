a360b945184a734d862f377b8d3fdfeba64f6c3e
CrazySt <bazhenov1994@gmail.com>
2018-10-12 12:00:22 +0300

    added link to the last_item. Rewrited several methods

--- app/doublelinked_list.py
+++ app/doublelinked_list.py
@@ -1,24 +1,34 @@
+"""DoubleLInked list with Items. what can i add?"""
+
 
 class Item:
+    """Item class for list"""
     def __init__(self, elem):
+        """this is constructor"""
         self.elem = elem
         self.next_item = None
         self.prev_item = None
 
     def next(self):
+        """return link to the next Item"""
         return self.next_item
 
     def prev(self):
+        """returns link to the previous Item"""
         return self.prev_item
 
 
 class EmptyListException(Exception):
+    """custom exception for empty list"""
     def __init__(self):
+        """initialazing of custom exception for empty list"""
         Exception.__init__(self, "This double_linked_list is empty!")
 
 
 class DoubleLinkedList:
+    """class of list"""
     def __init__(self, *args):
+        """constructor"""
         if args:
             j = 0
             for i in args:
@@ -31,38 +41,39 @@ class DoubleLinkedList:
                     item1.next_item = item2
                     item2.prev_item = item1
                     item1 = item2
+            self.last_item = item1
         else:
             self.first_item = None
+            self.last_item = None
 
     def last(self):
-        if self.first_item:
-            i = self.first_item
-            while i.next_item:
-                _ = i.next_item
-                i = _
+        """returns last element in list"""
+        if self.last_item:
+            return self.last_item.elem
         else:
             raise EmptyListException()
-        return i.elem
 
     def first(self):
+        """returns first element in list"""
         if self.first_item:
             return self.first_item.elem
         else:
             raise EmptyListException()
 
     def push(self, something):
+        """adds something in the end of list"""
         new_elem = Item(something)
-        if self.first_item:
-            i = self.first_item
-            while i.next_item:
-                i = i.next_item
-            i.next_item = new_elem
-            new_elem.prev_item = i
+        if self.last_item:
+            self.last_item.next_item = new_elem
+            new_elem.prev_item = self.last_item
+            self.last_item = new_elem
         else:
+            self.last_item = new_elem
             self.first_item = new_elem
         return True
 
     def length(self):
+        """returns amount of Items in the list"""
         j = 0
         if self.first_item:
             i = self.first_item
@@ -73,6 +84,7 @@ class DoubleLinkedList:
         return j
 
     def contains(self, query):
+        """checks if list contains some element"""
         if self.first_item:
             i = self.first_item
             while i:
@@ -83,39 +95,43 @@ class DoubleLinkedList:
         return False
 
     def pop(self):
-        if self.first_item:
-            last = self.first_item
-            while last.next_item:
-                last = last.next_item
+        """deletes and returns last element of the list"""
+        if self.last_item:
+            last = self.last_item
             if last is self.first_item:
                 self.first_item = None
+                self.last_item = None
             else:
                 pre = last.prev_item
                 pre.next_item = None
+                self.last_item = pre
             return last.elem
         else:
             raise EmptyListException()
 
     def delete(self, query):
+        """deletes and returns query element of the list if it is"""
         i = self.first_item
         if i:
             while i:
                 if i.elem == query:
                     prev = i.prev_item
-                    next_i = i.next_item
+                    next = i.next_item
                     if prev:
-                        prev.next_item = next_i
+                        prev.next_item = next
                     else:
-                        self.first_item = next_i
-                    if next_i:
-                        next_i.prev_item = prev
-
+                        self.first_item = next
+                    if next:
+                        next.prev_item = prev
+                    else:
+                        self.last_item = prev
                     return True
                 i = i.next_item
             return False
-        return False
+        raise EmptyListException()
 
     def unshift(self, new_elem):
+        """inserts new Item with new_elem in the beginnng of the list"""
         if self.first_item:
             i = Item(new_elem)
             i.next_item = self.first_item
@@ -125,20 +141,14 @@ class DoubleLinkedList:
         return True
 
     def shift(self):
+        """deletes and returns first element of the list"""
         if self.first_item:
             i = self.first_item
             self.first_item = i.next_item
             if self.first_item:
                 self.first_item.prev_item = None
+            else:
+                self.last_item = None
             return i.elem
         else:
             raise EmptyListException()
-
-    def make_list(self):
-        result = list()
-        i = self.first_item
-        while i:
-
-            result.append(i.elem)
-            i = i.next_item
-        return result
--- tests/tests_doublelinked_list.py
+++ tests/tests_doublelinked_list.py
@@ -5,6 +5,7 @@ from app import doublelinked_list as dll
 class DLLMethodsTest(unittest.TestCase):
 
     def test_init(self):
+        """test of doublelinked_list.init()"""
         i = dll.DoubleLinkedList()
         self.assertEqual(i.first_item, None)
         i = dll.DoubleLinkedList(1)
@@ -13,6 +14,7 @@ class DLLMethodsTest(unittest.TestCase):
         self.assertEqual(i.first_item.prev_item, None)
 
     def test_pop(self):
+        """test of doublelinked_list.pop()"""
         i = dll.DoubleLinkedList('w', 'q', 'e')
         self.assertEqual(i.pop(), 'e')
         self.assertEqual(i.pop(), 'q')
@@ -21,6 +23,7 @@ class DLLMethodsTest(unittest.TestCase):
             i.pop()
 
     def test_shift(self):
+        """test of doublelinked_list.shift()"""
         i = dll.DoubleLinkedList('w', 'q', 'e')
         self.assertEqual(i.shift(), 'w')
         self.assertEqual(i.shift(), 'q')
@@ -29,27 +32,32 @@ class DLLMethodsTest(unittest.TestCase):
             i.shift()
 
     def test_delete(self):
+        """test of doublelinked_list.delete()"""
         i = dll.DoubleLinkedList('w', 'q', 'e', 'e')
         self.assertEqual(i.delete('e'), True)
         self.assertEqual(i.delete('e'), True)
         self.assertEqual(i.delete('e'), False)
 
     def test_unshift(self):
+        """test of doublelinked_list.unshift()"""
         i = dll.DoubleLinkedList(2, 3)
         self.assertEqual(i.unshift(1), True)
         self.assertEqual(i.first_item.elem, 1)
 
     def test_push(self):
+        """test of doublelinked_list.push()"""
         i = dll.DoubleLinkedList(12)
         self.assertEqual(i.push(14), True)
         self.assertEqual(i.first_item.next_item.elem, 14)
 
     def test_contains(self):
+        """test of doublelinked_list.contains()"""
         i = dll.DoubleLinkedList('w', 'q', 'e')
         self.assertEqual(i.contains('e'), True)
         self.assertEqual(i.contains('r'), False)
 
     def test_first(self):
+        """test of doublelinked_list.first()"""
         i = dll.DoubleLinkedList('w', 'q', 'e')
         self.assertEqual(i.first(), 'w')
         i = dll.DoubleLinkedList()
@@ -57,6 +65,7 @@ class DLLMethodsTest(unittest.TestCase):
             i.first()
 
     def test_last(self):
+        """test of doublelinked_list.last()"""
         i = dll.DoubleLinkedList('w', 'q', 'e')
         self.assertEqual(i.last(), 'e')
         i = dll.DoubleLinkedList()
@@ -64,6 +73,7 @@ class DLLMethodsTest(unittest.TestCase):
             i.last()
 
     def test_length(self):
+        """test of doublelinked_list.length()"""
         i = dll.DoubleLinkedList('w', 'q', 'e')
         self.assertEqual(i.length(), 3)
         i = dll.DoubleLinkedList()
